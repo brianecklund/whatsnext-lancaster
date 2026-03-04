eb756a6d23cc81c992f7ee8b26b40e6ef6138dbb
brianecklund <brianecklund@gmail.com>
2026-03-03 22:28:49 -0500

    v59

--- app/locations/LocationsSplitClient.tsx
+++ app/locations/LocationsSplitClient.tsx
@@ -358,7 +358,7 @@ export default function LocationsSplitClient({ locations }: { locations: Locatio
 }
 
 function LocationDetail({ location }: { location: LocationRow }) {
-  const detailFlashKey = location.id ?? location.uid ?? location.slug ?? location.name ?? "detail";
+  const detailFlashKey = location.id ?? location.uid ?? location.name ?? "detail";
   return (
     <div key={detailFlashKey} className="detailCard detailFlash">
       <div className="detailTitle fadeInItem" style={{ animationDelay: "260ms" }}>
